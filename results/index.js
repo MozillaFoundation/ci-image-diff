8ca027a5ccc8e9b08b7445852127886ba524bb51
Pomax <pomax@nihongoresources.com>
2021-05-17 13:22:06 -0700

    preview update

--- results/index.js
+++ results/index.js
@@ -1,6 +1,7 @@
 const pageArguments = new URLSearchParams(window.location.search);
 const referenceId = pageArguments.get(`reference`);
 const compareId = pageArguments.get(`compare`);
+const create = document.createElement.bind(document);
 
 // Fetch the diff list for this comparison
 (async function () {
@@ -8,22 +9,68 @@ const compareId = pageArguments.get(`compare`);
   Object.entries(data).forEach(processDiffs);
 })();
 
+/**
+ * ... docs go here...
+ */
 function processDiffs([browserWidth, difflist]) {
+  const count = difflist.length;
+
+  if (!count) return;
+
+  const option = create(`option`);
+  option.value = browserWidth;
+  option.textContent = browserWidth;
+  diffsets.append(option);
+
+  const [browser, width] = browserWidth.split('-');
+
+  const showOrNot = create(`div`);
+  showOrNot.classList.add(`show-or-not`);
+  diffs.append(showOrNot);
+
+  const toggle = create(`input`);
+  toggle.type = `checkbox`;
+  toggle.setAttribute(`checked`, `checked`)
+  toggle.id = browserWidth;
+  toggle.addEventListener(`change`, evt => {
+    const action = evt.target.checked ? `remove` : `add`;
+    section.classList[action](`hidden`);
+  });
+  showOrNot.append(toggle);
+
+  const label = create(`label`);
+  label.setAttribute(`for`, browserWidth);
+  label.textContent = `${browser} - ${width}px (${count} diff${count>1 ? `s` : ``})`
+  showOrNot.append(label);
+
+  const section = create(`section`);
+  diffs.append(section);
+
   const imageSets = difflist.map((diff) => buildImage(browserWidth, diff));
-  imageSets.forEach(processImageSet);
+  imageSets.forEach(set => processImageSet(section, set));
 }
 
-function processImageSet(set) {
-  const section = document.createElement(`section`);
-  section.innerHTML = `
-    <h2>${set.diff}</h2>
+/**
+ * ... docs go here...
+ */
+function processImageSet(section, set) {
+  const specificDiff = create(`section`);
+  specificDiff.classList.add(`specific-diff`);
+  specificDiff.innerHTML = `
+    <h2>Diff for URL: ${set.diff}</h2>
   `;
-  section.append(set.original);
-  section.append(set.compare);
-  reference.append(section);
+  const figureSet = create(`div`);
+  figureSet.classList.add(`figure-set`)
+  figureSet.append(set.original);
+  figureSet.append(set.compare);
+  specificDiff.append(figureSet);
+  section.append(specificDiff);
 }
 
-function buildImage(browserWidth, diff) {
+/**
+ * ... docs go here...
+ */
+ function buildImage(browserWidth, diff) {
   const original = `./${referenceId}/${browserWidth}/${diff}/screenshot.png`;
   const diffBase = `./${compareId}/${browserWidth}/${diff}`;
   const compare = `${diffBase}/screenshot.png`;
@@ -31,22 +78,32 @@ function buildImage(browserWidth, diff) {
   const diffMask = `${diffBase}/diff_mask.png`;
 
   return {
-    diff: diff,
+    diff,
+    browserWidth,
     original: buildImageElement(original, originalMask, "reference"),
     compare: buildImageElement(compare, diffMask, "compare"),
   };
 }
 
-function buildImageElement(screenshot, overlay, classes='') {
-  const figure = document.createElement(`figure`);
+/**
+ * ... docs go here...
+ */
+ function buildImageElement(screenshot, overlay, classes='') {
+  const figure = create(`figure`);
   figure.setAttribute(`style`, `
     background-image: url("${screenshot}");
   `);
 
-  const img = new Image();
+  let img = new Image();
+  img.src = screenshot;
+  img.style.width = `100%`;
+  img.classList.add(`for-sizing-only`);
+  figure.append(img);
+
+  img = new Image();
   img.src = overlay;
   img.style.width = `100%`;
-  img.classList.add(classes);
+  img.classList.add(classes, `overlay`);
   figure.append(img);
   return figure;
 }
